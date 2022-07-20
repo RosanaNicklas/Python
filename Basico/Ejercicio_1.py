67afd26cbfb487fc2a4b2e30656bfd6913fffa37
RosanaNicklas <rosana8longares@gmail.com>
2022-07-20 11:49:27 +0200

    Ejercicio2

--- Basico/Ejercicio_1.py
+++ Basico/Ejercicio_1.py
@@ -1,12 +1,19 @@
 #Ejercicio 1
 
 #DESCRIBE UNA VARIABLE CON NOMBRE "Notas" QUE TENGA EL VALOR -3.
+from decimal import BasicContext
+
+
 notas = -3
 print(notas)
 
-#EJERCICIO 2
+""" Ejercicio 2
+Imprime los valores de "s" es igual 25, de "y" es igual a 10, poniendo la siguiente salida: El valor de "s" es "valor de s" y el valor de y es
+"valor de y """
 
-"""
-Imprime valores 
-"""
-4 * 4
+s = 25
+y = 10
+print(f'el valor de s es {s} y el valor de y es {y}')
+print('el valor de s es ' + str(s) + ' y el valor de y es ' + str(y))
+print('el valor de s es ', s, ' y el valor de y es ', y)
+print(f'el valor de s es %s y el valor de y es %s' %(s, y))
